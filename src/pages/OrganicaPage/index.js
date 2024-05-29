85e0f4b1c64bae81fe5b8e237670d20aa99dee57
Miquel <miquelmuntanerweb@gmail.com>
2024-05-29 09:40:57 +0200

    Improving the representation of the organic molecules

--- src/pages/OrganicaPage/index.js
+++ src/pages/OrganicaPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { PageLayout } from '../../components/PageLayout'
 import { ContainerDiv, InputText, StyledForm, Canvas } from './styles'
 import { Header } from '../../components/Header'
@@ -6,24 +6,37 @@ import { organicProcessor } from '../../tools/organicProcessor'
 
 export const OrganicaPage = ({ setDarkMode }) => {
     const [SMILES, setSMILES] = useState("")
+    const [inputValue, setInputValue] = useState("")
 
     // Definint constants
-    // ADVERTENCIA: no modificar els valors per defecta sense entendre el funcionament del programa
+    // ADVERTENCIA: no modificar els valors per defecte sense entendre el funcionament del programa
 
     const STROKE = "black" // Estils de la línia
     const LINE_WIDTH = 2 // Grossor de la línia
-    const CANVAS = document.getElementById("mainOrganicCanvas") // Etiqueta de canvas
-    const CTX = CANVAS.getContext("2d") // Context del canvas
+    let CANVAS  // Etiqueta de canvas
+    let CTX // Context del canvas
     const DOUBLE_BOND_SPACING = 12 // Espai entra la línia princpial i la línia del doble enllaç (px)
     const LINE_LENGTH = 100 // Longitud de la línia que representa els carbonis (px)
     const DEFAULT_ANGLE = Math.PI / 5 // Angle per defecta entre les línies (menys en cas de raminificacions) (rad)
 
 
+    useEffect(() => {
+        CANVAS = document.getElementById("mainOrganicCanvas")
+        CTX = CANVAS.getContext("2d")
+    }, [SMILES])
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        setInputValue(e.target[0].value)
         let result = organicProcessor(e.target[0].value)
-        let smiles_output = ""
+
         console.log(result)
+        drawMolecule(result)
+        generateSMILES(result)
+    }
+
+    const generateSMILES = (result) => {
+        let smiles_output = ""
 
         for (let i = 0; i < result[0].carbons; i++) {
 
@@ -50,72 +63,97 @@ export const OrganicaPage = ({ setDarkMode }) => {
                 + (result[0].ciclo === true && i == result[0].carbons - 1 ? "1" : "")
         }
 
-        drawMolecule(result)
-
-
         setSMILES(smiles_output)
     }
 
-    const drawLine = (x1, y1, x2, y2) => {
+    const drawLine = (x0, y0, x1, y1) => {
         CTX.beginPath();
-        CTX.moveTo(x1, y1);
-        CTX.lineTo(x2, y2);
+        CTX.moveTo(x0, y0);
+        CTX.lineTo(x1, y1);
         CTX.strokeStyle = STROKE;
         CTX.LINE_WIDTH = LINE_WIDTH;
         CTX.stroke();
     }
 
-    const drawCarbon = (x1, y1, angle, bond = 1) => {
+    const drawCarbon = (x0, y0, angle, bond = 1) => {
         let temp_double_bond_spacing = DOUBLE_BOND_SPACING
 
         if (angle >= 0) {
             temp_double_bond_spacing = temp_double_bond_spacing * Math.tan(Math.PI / 2 - angle)
         }
 
-        var x2 = Math.cos(angle) * LINE_LENGTH + x1
-        var y2 = Math.sin(angle) * LINE_LENGTH + y1
+        var x1 = Math.cos(angle) * LINE_LENGTH + x0
+        var y1 = Math.sin(angle) * LINE_LENGTH + y0
 
-        drawLine(x1, y1, x2, y2)
+        // Dibuixant la línia principal
+        drawLine(x0, y0, x1, y1)
 
         if (bond == 2 || bond == 3) {
+
             if (angle <= 0) {
-                var x3 = Math.cos(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + x1
-                var y3 = Math.sin(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + (y1 - temp_double_bond_spacing)
+                var x2 = Math.cos(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + x0
+                var y2 = Math.sin(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + (y0 - temp_double_bond_spacing)
             } else {
-                var x3 = Math.cos(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + (x1 + temp_double_bond_spacing)
-                var y3 = Math.sin(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + y1
+                var x2 = Math.cos(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + (x0 + temp_double_bond_spacing)
+                var y2 = Math.sin(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + y0
             }
 
-            let bondX1 = x1 + (angle >= 0 ? temp_double_bond_spacing : 0)
-            let bondY1 = y1 - (angle <= 0 ? temp_double_bond_spacing : 0)
-            drawLine(bondX1, bondY1, x3, y3)
+            let bondx0 = x0 + (angle >= 0 ? temp_double_bond_spacing : 0)
+            let bondy0 = y0 - (angle <= 0 ? temp_double_bond_spacing : 0)
+            drawLine(bondx0, bondy0, x2, y2)
         }
 
         if (bond == 3) {
+
             if (angle <= 0) {
-                var x3 = Math.cos(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + x1
-                var y3 = Math.sin(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + (y1 - temp_double_bond_spacing)
+                temp_double_bond_spacing = -DOUBLE_BOND_SPACING * Math.tan(Math.PI / 2 - angle)
             } else {
-                var x3 = Math.cos(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + (x1)
-                var y3 = y2
+                temp_double_bond_spacing = DOUBLE_BOND_SPACING
             }
 
-            let bondX1 = x1 - (angle <= 0 ? temp_double_bond_spacing : 0)
-            let bondY1 = y1 + (angle >= 0 ? temp_double_bond_spacing : 0)
-            drawLine(bondX1, bondY1, x3, y3)
+            console.log(angle)
+            console.log(temp_double_bond_spacing)
+            if (angle <= 0) {
+                console.log("aqui")
+                var x2 = x1
+                //var y2 = Math.sin(angle) * (LINE_LENGTH - 2 * DOUBLE_BOND_SPACING) + (y0 - temp_double_bond_spacing)
+                var y2 = y1 - Math.tan(angle) * temp_double_bond_spacing
+            } else {
+                var x2 = x1 - (temp_double_bond_spacing / Math.tan(angle))
+                var y2 = y1
+            }
 
+            let bondx0 = x0 + (angle <= 0 ? temp_double_bond_spacing : 0)
+            let bondy0 = y0 + (angle >= 0 ? temp_double_bond_spacing : 0)
+            drawLine(bondx0, bondy0, x2, y2)
         }
 
 
-        return [x2, y2]
+        return [x1, y1]
     }
 
     const drawMolecule = (data) => {
-        CTX.clearRect(0, 0, 1080, 1080)
+
+        // CTX.clearRect(0, 0, 1080, 1080)
+        CTX.fillStyle = "white";
+        CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
         const TOTAL_LENGTH = Math.cos(DEFAULT_ANGLE) * 100 * (data[0].carbons - 1)
         const CICLO_ANGLE = 2 * Math.PI / data[0].carbons
 
+        //drawLine(540, 0, 540, 1080)
+        //drawLine(0, 540, 1080, 540)
+
         let last_coordinates = [CANVAS.width / 2 - TOTAL_LENGTH / 2, CANVAS.height / 2]
+        if (data[0].ciclo == true) {
+            let rad_circumcicle = (LINE_LENGTH / (2 * Math.sin(Math.PI / (data[0].carbons - 1))))
+            console.log(rad_circumcicle)
+            last_coordinates = [
+                (CANVAS.width / 2) + LINE_LENGTH / 2,
+                (CANVAS.height / 2) - rad_circumcicle
+            ]
+        }
+
+        console.log(last_coordinates)
         let angle = - DEFAULT_ANGLE
         let ciclo_angle_acumulated = CICLO_ANGLE
 
@@ -131,30 +169,22 @@ export const OrganicaPage = ({ setDarkMode }) => {
                 )
             )
 
-            /*
-            if (data[0].doubleBonds.includes(i + 1)) {
-                last_coordinates = drawCarbon(last_coordinates[0], last_coordinates[1], angle, 2)
-            } else if (data[0].tripleBonds.includes(i + 1)) {
-                last_coordinates = drawCarbon(last_coordinates[0], last_coordinates[1], angle, 3)
-            } else {
-                last_coordinates = drawCarbon(last_coordinates[0], last_coordinates[1], angle)
-            }
-            */
-
+            // Cercant ramificacions, en cas de que n'hi hagi es dibuixa
             for (let k in data[1]) {
 
                 for (let j in data[1][k].position) {
 
                     if (data[1][k].position[j] - 2 === i) {
-                        let angle_ramificacio = (angle <= 0 ? -Math.PI / 2 + DEFAULT_ANGLE : Math.PI / 2 + DEFAULT_ANGLE)
-                        let change_in_angle = true
+                        let angle_ramificacio = (angle <= 0 ? -Math.PI / 2 + DEFAULT_ANGLE : Math.PI / 2 + DEFAULT_ANGLE) - ciclo_angle_acumulated
+                        let change_angle = true // Vertader si l'angle per defecta ha de ser invertit
+
+                        // Dibuixam la primera línia verticalment i tening en compte cap a quina direcció
                         let last_coordinates_ramificacio = drawCarbon(last_coordinates[0], last_coordinates[1], angle_ramificacio - DEFAULT_ANGLE)
 
                         for (let z = 1; z < data[1][k].carbons; z++) {
                             last_coordinates_ramificacio = drawCarbon(last_coordinates_ramificacio[0], last_coordinates_ramificacio[1], angle_ramificacio)
-                            angle_ramificacio = (change_in_angle != true ? DEFAULT_ANGLE : - DEFAULT_ANGLE) + (angle <= 0 ? -Math.PI / 2 : Math.PI / 2)
-                            change_in_angle = !change_in_angle
-                            console.log(last_coordinates_ramificacio)
+                            angle_ramificacio = (change_angle != true ? DEFAULT_ANGLE : - DEFAULT_ANGLE) + (angle <= 0 ? -Math.PI / 2 : Math.PI / 2)
+                            change_angle = !change_angle
                         }
                     }
                 }
@@ -167,23 +197,45 @@ export const OrganicaPage = ({ setDarkMode }) => {
         if (data[0].ciclo == true) {
             drawCarbon(last_coordinates[0], last_coordinates[1], ciclo_angle_acumulated)
         }
+
+        drawWaterMark()
+    }
+
+    const drawWaterMark = () => {
+        var text = "M. Muntaner"
+        CTX.font = "bold 35px Roboto"
+        CTX.fillStyle = "#3A4B4C"
+        CTX.fillText(text, 80, 1000)
+    }
+
+    const downloadButtonPressed = () => {
+        var link = document.createElement('a');
+        link.download = `${inputValue}.png`;
+        link.href = CANVAS.toDataURL()
+        link.click();
     }
 
     return (
         <PageLayout setDarkMode={setDarkMode}>
             <ContainerDiv>
-                <Header subheader="Orgànica" />
-                <StyledForm onSubmit={handleSubmit}>
-                    <label htmlFor="formula" className='form_label' id='inorganica_label'>Nom</label>
-                    <InputText type="text" />
-                    <input type="submit" value="Executar"></input>
-                </StyledForm>
-                <p style={{ fontWeight: "normal" }}>SMILES output: <b>{SMILES}</b></p>
-                <Canvas id='mainOrganicCanvas' width="1080" height="1080" style={{ backgroundColor: "white" }}></Canvas>
-                {/*<img src={`http://hulab.rxnfinder.org/smi2img/${encodeURIComponent(SMILES)}/?width=500&height=200`} alt="" />*/}
-                {/*<img src={`https://cactus.nci.nih.gov/chemical/structure/${encodeURIComponent(SMILES)}/image?width=250&height=250`}></img>
-                <script type="text/javascript" src={`https://chemapps.stolaf.edu/jmol/jmol.php?model=${encodeURIComponent(SMILES)}&inline`}></script>
-    <script type="text/javascript" src={`https://chemapps.stolaf.edu/jmol/jmol.php?model=${encodeURIComponent(SMILES)}&image2d&inline`}></script>*/}
+                <div>
+                    <Header subheader="Orgànica" />
+                    <StyledForm onSubmit={handleSubmit}>
+                        <label htmlFor="formula" className='form_label' id='inorganica_label'>Nom</label>
+                        <InputText type="text" />
+                        <input type="submit" value="Executar"></input>
+                    </StyledForm>
+                    {SMILES !== "" ?
+                        <>
+                            <p style={{ fontWeight: "normal" }}>Nomenclatura SMILES: <b>{SMILES}</b></p>
+                            <button onClick={downloadButtonPressed}>Descarrega</button>
+                        </>
+                        : <div style={{ height: "91px" }} />
+                    }
+                </div>
+                <div>
+                    <Canvas id='mainOrganicCanvas' width="1080" height="1080" style={{ backgroundColor: "white" }}></Canvas>
+                </div>
             </ContainerDiv>
         </PageLayout >
     )
--- src/pages/OrganicaPage/styles.js
+++ src/pages/OrganicaPage/styles.js
@@ -2,33 +2,64 @@ import styled from "styled-components"
 
 export const ContainerDiv = styled.div`
     height: calc(100% - 79px - 34px);
+    width: 100vw !important;
     text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    display: grid;
+    grid-template-columns: 12% 38% auto 432px 12%;
     align-content: center;
-    gap: 0;
+    justify-content: space-between;
     width: 50vw;
     padding-top: 34px;
-    margin: 0 25% 10% 25%;
     color: white;
     font-weight: bold;
     font-size: 25px;
     
     & > div {
-        width: 100%;
         color: white;
         font-size: 1.3rem;
-        text-align: left;
         margin-top: 15px;
     }
+
+    
+    & > div:nth-child(2) {
+        //width: 432px;
+        height: 432px;
+        grid-column-start: 4;
+        grid-column-end: 4;
+        margin: 0;
+    }
+
+    & > div:first-child > button {
+        width: 100px;
+        height: 40px;
+        border-radius: 0;
+        background-color: transparent;
+        border: 2px solid #9b9b9b;
+        color: #9b9b9b;
+    }
+
+    & > div:first-child > button:hover {
+        border-image-slice: 1;
+        border-image-source: linear-gradient(#11998e, #38ef7d);
+        color: rgba(255, 255, 255, 0.8);
+    }
+
+    & > div:first-child {
+        grid-column-start: 2;
+        grid-column-end:2;
+        align-items: flex-start;
+        text-align: left;
+    }
+    // SMILES
+    & > div:first-child > p {
+        margin: 15px 0 15px 0;
+    } 
 `
 
 export const StyledForm = styled.form`
     position: relative;
     padding: 15px 0 0;
     margin-top: 10px;
-    width: 100%;
     display: flex;
 
     & > label {
@@ -103,9 +134,11 @@ export const InputText = styled.input`
 
 export const Canvas = styled.canvas`
     transform: scale(0.4);
-    display: flex;
-    position: relative;
-    top: -324px;
+    -ms-transform: scale(0.4); 
+    -webkit-transform: scale(0.4);
+    transform-origin: top left;
+    display: block;
+    height: auto;
     padding: 0;
     margin: 0;
 `
